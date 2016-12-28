df7548ea93418fb1e9abca6734ca9c8e39e190b8
Danny <cirlmai@gmail.com>
2016-12-28 17:58:47 +0800

    Create Project

---
+++ app/src/main/java/com/example/danny/customviewexample/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.danny.customviewexample;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.danny.customviewexample.CustomView.TestView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+
+        final TestView pie = (TestView) this.findViewById(R.id.testView);
+        pie.addItem("Agamemnon", 2, res.getColor(R.color.seafoam));
+        pie.addItem("Bocephus", 3.5f, res.getColor(R.color.chartreuse));
+        pie.addItem("Calliope", 2.5f, res.getColor(R.color.emerald));
+        pie.addItem("Daedalus", 3, res.getColor(R.color.bluegrass));
+        pie.addItem("Euripides", 1, res.getColor(R.color.turquoise));
+        pie.addItem("Ganymede", 3, res.getColor(R.color.slate));
+
+        findViewById(R.id.btnSet).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                pie.setCurrentItem(0);
+            }
+        });
+    }
+}
